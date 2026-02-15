93439fd25af36d18e3a821b19adbe9e0dd5b7eb8
Henrique Almeida <rick.vieira.almeida@gmail.com>
2026-02-14 22:18:50 -0300

    feat: add all entities with their relationships

---
+++ src/BattleQuest.Domain/Entities/Channel.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleQuest.Domain.Entities
+{
+	public class Channel
+	{
+		public int ChannelId { get; set; }
+		public required string Name { get; set; }
+
+		public ICollection<Event> Events { get; set; } = new List<Event>();
+	}
+}
---
+++ src/BattleQuest.Domain/Entities/Quest.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleQuest.Domain.Entities
+{
+    public class Quest
+    {
+        public required string QuestId { get; set; }
+        public string? Name { get; set; }
+
+		public ICollection<Event> Events { get; set; } = new List<Event>();
+	}
+}
